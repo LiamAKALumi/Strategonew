109798fc3fcf50d043503fb09ad179e722b060ba
AllTeach <33581448+AllTeach@users.noreply.github.com>
2024-04-06 10:17:18 +0300

    Example for Stratego

--- src/main/java/com/example/stratego/GameView.java
+++ src/main/java/com/example/stratego/GameView.java
@@ -63,7 +63,7 @@ public class GameView extends Application {
         launch();
     }
 
-
+// example
     public void updateView(Piece[][] board) {
         for (int x = 0; x < NUM_CELLS; x++) {
             for (int y = 0; y < NUM_CELLS; y++) {
